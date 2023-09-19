bc5ff33362f44b84bbcfa89a689a58225dd7efce
Ashish Singh <32197380+ashishsingh0307@users.noreply.github.com>
2023-09-19 07:16:53 +0000

    datatypes learned

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer vesrion
+
+// alert(3+3);
+// alert("hello");
+
+/* 
+    we are using nodejs so we use alert in different manner
+    if we use browser then we can use alert.
+*/
+
+console.log(3 + 3);
+console.log("Ashish");
+
+// code readability should be high
+
+let name = "Ashish";
+let age = 16;
+let iLoggedIn = false;
+
+// Premitive Data Types
+// Number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined.
+// symbol => unique
+
+// Non Premitive Data Types
+// Objects 
+
+
+console.log(typeof undefined);
+console.log(typeof null);
