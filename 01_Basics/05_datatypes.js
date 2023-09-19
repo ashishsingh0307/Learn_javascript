f89156c4b2541a76fd6968aebe4e7800db3bdc86
Ashish Singh <32197380+ashishsingh0307@users.noreply.github.com>
2023-09-19 17:24:16 +0000

    comparison operators learned

---
+++ 01_Basics/05_datatypes.js
@@ -0,0 +1,23 @@
+// COMPARISON OPERATORS
+
+console.log(2 > 1); // true
+console.log(2 >= 1); // true
+console.log(2 < 1); // false
+console.log(2 <= 1); // false
+console.log(2 == 1); // false
+console.log(2 != 1); // true
+
+console.log("2" > 1); // true
+console.log("02" > 1); // true
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+/*
+
+    The Reason is that an equality check '==' and comparisons '> < >= <=' work differently. Because
+    Comparisons convert null to a number, treating it as 0.
+    That's why (3) null>=0 is true and (1) null>0 is false.
+
+*/
