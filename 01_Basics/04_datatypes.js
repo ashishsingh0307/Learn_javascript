a7e697ce5d3a662134cd92a32af9b7bea0c025d5
Ashish Singh <32197380+ashishsingh0307@users.noreply.github.com>
2023-09-19 12:10:10 +0000

    operations learned

---
+++ 01_Basics/04_datatypes.js
@@ -0,0 +1,44 @@
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// =================== OPERATIONS ======================= //
+
+let value = 3
+let negValue = -value
+console.log(negValue); // -3
+ 
+console.log(2+2); // 4
+console.log(2-2); // 0
+console.log(2*2); // 4
+console.log(2**2); // 4
+console.log(2/3); // 0.66
+console.log(2%3); // 2
+
+let str1 = "hello"
+let str2 = " Ashish"
+
+let str3 = str1 + str2
+console.log(str3); // hello Ashish
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122 ==> first parameter is string thats why it behaved like string and concatinated it.
+console.log(1 + 2 + "2"); // 32 ==> first parameter is number thats why it added 1 and 2 and concatinated last 2 as a string.
+
+console.log(true); // true
+console.log(+true); // 1
+// console.log(true+); // error
+console.log(+""); // 0
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++; // POSTFIX OPERATOR
+++gameCounter; // PREFIX OPERATOR
+console.log(gameCounter);
+
