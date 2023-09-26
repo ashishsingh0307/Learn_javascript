0f1ddea9529a7897e1b861ed7e0b08ec6be5ffd5
Ashish Singh <32197380+ashishsingh0307@users.noreply.github.com>
2023-09-26 10:00:21 +0000

    stack and heap learned

--- 02_Memory/01_memory.js
+++ 02_Memory/01_memory.js
@@ -4,7 +4,8 @@
 
         1.) STACK (PRIMITIVE)
         
-
+            Stack is used for primitive datatypes which stacks values over values or variables over variable. In stack when we assign variable 1 value to other variable 2. for instance:-
+            1) first 
 
         2.) HEAP (NON-PRIMITIVE)
 
