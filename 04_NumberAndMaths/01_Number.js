53f56738d0d46627a16ffed83141884672fc08aa
Ashish Singh <singh.ashish041@gmail.com>
2023-09-28 08:16:20 +0530

    started leaning numbers and maths

---
+++ 04_NumberAndMaths/01_Number.js
@@ -0,0 +1,8 @@
+const score = 400;
+console.log(score);
+// EXPLICIT NUMBER DEFINATION
+const balance = new Number(100);
+console.log(balance);
+
+// CONVERTING NUMBER TO STRING
+console.log(balance.toString());
