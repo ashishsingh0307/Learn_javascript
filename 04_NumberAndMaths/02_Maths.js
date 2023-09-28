557da5cd455025c7f593cb0bc0bc47079d74f34e
Ashish Singh <singh.ashish041@gmail.com>
2023-09-28 09:03:45 +0530

    learned about round function, ceil and floor

--- 04_NumberAndMaths/02_Maths.js
+++ 04_NumberAndMaths/02_Maths.js
@@ -1,2 +1,12 @@
 console.log(Math);
-console.log(Math.abs(-4)); // THIS IS TO CONVERT THE NEGATIVE NUMBER TO A POSITIVE NUMBER && POSITIVE NUMBER REMAINS POSITIVE NUMBER
+
+// THIS IS TO CONVERT THE NEGATIVE NUMBER TO A POSITIVE NUMBER && POSITIVE NUMBER REMAINS POSITIVE NUMBER
+console.log("This is the aboslute function value for the number -4: " + Math.abs(-4));
+
+// THIS FUNCTION IN MATH ROUNDS OF THE VALUE JUST LIKE WE DO IN DAILY LIFE
+console.log("This is the round function value for the number 4.4 : " + Math.round(4.4));
+console.log("This is the round function value for the number 4.6: " + Math.round(4.6));
+
+// BUT IF WE WANT TO CONTROL OVER WHEN TO GET UPPER VALUE OR LOWER VALUE WE DO THE FOLLOWING
+console.log("This is the aboslute function value for the number 4.2: " + Math.ceil(4.2));
+console.log("This is the aboslute function value for the number 4.8: " + Math.floor(4.8));
