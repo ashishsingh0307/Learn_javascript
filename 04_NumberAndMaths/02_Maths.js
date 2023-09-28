7d57175a363116fa4b8ccbae610e9a974fe0d5da
Ashish Singh <singh.ashish041@gmail.com>
2023-09-28 19:44:45 +0530

    learned squareroot function

--- 04_NumberAndMaths/02_Maths.js
+++ 04_NumberAndMaths/02_Maths.js
@@ -10,3 +10,7 @@ console.log("This is the round function value for the number 4.6: " + Math.round
 // BUT IF WE WANT TO CONTROL OVER WHEN TO GET UPPER VALUE OR LOWER VALUE WE DO THE FOLLOWING
 console.log("This is the aboslute function value for the number 4.2: " + Math.ceil(4.2));
 console.log("This is the aboslute function value for the number 4.8: " + Math.floor(4.8));
+
+// IF WE WANT TO FIND THE SQUARE ROOT OF ANY NUMBER USING THE MATH LIBRARY OF JAVASCRIPT WE CAN DO THE FOLLOWING
+console.log("This is the square root of number 25 : " + Math.sqrt(25));
+console.log("This is the square root of number 25 : " + Math.sqrt(64));
