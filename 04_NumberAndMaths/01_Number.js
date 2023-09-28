d525a32384b53912eb28b73c946770617bd06b25
Ashish Singh <singh.ashish041@gmail.com>
2023-09-28 08:29:57 +0530

    leaning number and some predefined functions

--- 04_NumberAndMaths/01_Number.js
+++ 04_NumberAndMaths/01_Number.js
@@ -6,7 +6,12 @@ console.log(balance);
 
 // CONVERTING NUMBER TO STRING
 console.log(balance.toString());
+
 // CHECKING THE TYPE AFTER CONVERSION
 console.log(typeof(balance.toString()));
+
 // CHACKING THE LENGTH OF THE NUMBER
 console.log(balance.toString().length);
+
+// CONVERT NUMBER INTO DECIMAL UPTO 2 PLACE (2 is defined inside the toFixed() function)
+console.log(balance.toFixed(2));
