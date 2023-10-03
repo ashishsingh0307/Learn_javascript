695cab40b231a1ee886a3435b1f160a91f77c710
Ashish Singh <singh.ashish041@gmail.com>
2023-10-03 09:01:19 +0530

    learning about arrays more

---
+++ INTERMEDIATE/06_Arrays/02_arrays.js
@@ -0,0 +1,7 @@
+const marvelHeros = ["Thor", "Ironman", "Spiderman"];
+const dcHeros = ["Superman", "Flash", "Batman"];
+
+marvelHeros.push(dcHeros);
+
+console.log(marvelHeros);
+console.log(marvelHeros[3][1]);
