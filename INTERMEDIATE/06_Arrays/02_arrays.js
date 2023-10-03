0e8c52f017b8286d0dc4808cb780bd5d01d2ae3f
Ashish Singh <singh.ashish041@gmail.com>
2023-10-03 09:07:45 +0530

    learned about push and concat

--- INTERMEDIATE/06_Arrays/02_arrays.js
+++ INTERMEDIATE/06_Arrays/02_arrays.js
@@ -1,7 +1,10 @@
 const marvelHeros = ["Thor", "Ironman", "Spiderman"];
 const dcHeros = ["Superman", "Flash", "Batman"];
 
-marvelHeros.push(dcHeros);
+// marvelHeros.push(dcHeros); // PUSHES THE SECOND ARRAY INTO THE FIRST ARRAY. SECOND ARRAY WILL BE ADDED TO PREVIOUS ARRAY AND FIRST ARRAY WILL TREAT IT AS A WHOLE DATA LIKE WHOLE ARRAY WILL BE A SINGLE INDEX VALUE
 
-console.log(marvelHeros);
-console.log(marvelHeros[3][1]);
+// console.log(marvelHeros);
+// console.log(marvelHeros[3][1]);
+
+const allHeros = marvelHeros.concat(dcHeros); // THIS METHOD RETURN NEW ARRAY BY COMBINING THEM INTO ONE ARRAY.
+console.log(allHeros);
