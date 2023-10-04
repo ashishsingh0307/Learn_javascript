b2925e9ea29edf9dcb6ee63cc3b2df65f06e383d
Ashish Singh <singh.ashish041@gmail.com>
2023-10-04 08:57:05 +0530

    learning about assign function

--- INTERMEDIATE/07_Objects/02_objects.js
+++ INTERMEDIATE/07_Objects/02_objects.js
@@ -5,7 +5,7 @@ tinderUser.id = "123abc";
 tinderUser.name = "Sam";
 tinderUser.isLoggedIn = false;
 
-console.log(tinderUser); // OUTPUT IS SAME WHEN WE ARE MAKING OBJECTS WITH THE HELP OF CONSTRUCTOR
+// console.log(tinderUser); // OUTPUT IS SAME WHEN WE ARE MAKING OBJECTS WITH THE HELP OF CONSTRUCTOR
 
 const regularUser = {
     email : "sam@gmail.com",
@@ -17,8 +17,25 @@ const regularUser = {
     }
 }
 
-console.log(regularUser);
-console.log(regularUser.fullName);
-console.log(regularUser.fullName.userFullName);
-console.log(regularUser.fullName.userFullName.firstName);
+// console.log(regularUser);
+// console.log(regularUser.fullName);
+// console.log(regularUser.fullName.userFullName);
+// console.log(regularUser.fullName.userFullName.firstName);
 
+// MERGING OBJECTS
+const object1 = {
+    1: "a",
+    2: "b"
+}
+
+const object2 = {
+    3: "c",
+    4: "d"
+}
+
+const object3 = { object1, object2 };
+const object4 = { ...object1, ...object2 };
+const object5 = Object.assign(object1, object2);
+// console.log(object3);
+console.log("This is spread operator: ",object4);
+console.log("This is assign function: ",object5);
