1070857971a8070666b9175840949778d4a1c7c9
Ashish Singh <singh.ashish041@gmail.com>
2023-10-12 08:24:46 +0530

    learned about default parameter values

--- INTERMEDIATE/08_functions/01_function.js
+++ INTERMEDIATE/08_functions/01_function.js
@@ -20,14 +20,14 @@ function addTwoNumbers(number1, number2){
 // WHEN WE DEFINE A FUNCTION WE PASS PARAMETERS
 // WHEN WE CALL A FUNCTION WE PASS ARGUMENTS.
 
-addTwoNumbers(); // OUTPUT WILL BE NaN BECAUSE WE NEED TO GIVE 2 ARGUMENTS AS WE PASSED 2 PARAMETERS IN THE FUNCTION addTwoNumbers()
-addTwoNumbers(3, 4); // OUTPUT IS 7.
-addTwoNumbers(3, "4"); // OUTPUT WILL BE 34 AS JS CONVERTS BOTH THE ARGUMENTS INTO STRING AND CONCATINATE BOTH. IT IS HAPPENIGN BECAUSE OF TYPE CONVERSION
-addTwoNumbers(3, "a"); // OUTPUT WILL BE 3a.
+// addTwoNumbers(); // OUTPUT WILL BE NaN BECAUSE WE NEED TO GIVE 2 ARGUMENTS AS WE PASSED 2 PARAMETERS IN THE FUNCTION addTwoNumbers()
+// addTwoNumbers(3, 4); // OUTPUT IS 7.
+// addTwoNumbers(3, "4"); // OUTPUT WILL BE 34 AS JS CONVERTS BOTH THE ARGUMENTS INTO STRING AND CONCATINATE BOTH. IT IS HAPPENIGN BECAUSE OF TYPE CONVERSION
+// addTwoNumbers(3, "a"); // OUTPUT WILL BE 3a.
 
-const result = addTwoNumbers(4, 7);
+// const result = addTwoNumbers(4, 7);
 
-console.log("result : ", result); // IT IS BECAUSE OUR FUNCTION IS NOT RETURNING ANYTHING TO BE STORED.
+// console.log("result : ", result); // IT IS BECAUSE OUR FUNCTION IS NOT RETURNING ANYTHING TO BE STORED.
 
 
 function sum(number1, number2){
@@ -37,7 +37,7 @@ function sum(number1, number2){
 }
 
 const answer = sum(3, 5); // THIS ANSWER VRIABLE IS OUTSIDE OF THE sum() FUNCTION.
-console.log("result1 : ", answer);
+// console.log("result1 : ", answer);
 
 
 function loginUserMessage(username){
@@ -47,11 +47,11 @@ function loginUserMessage(username){
 const notification1 = loginUserMessage("Ashish"); // OUTPUT : Ashish Just Logged In
 const notification2 = loginUserMessage(""); // OUTPUT :  Just Logged In
 const notification3 = loginUserMessage(); // OUTPUT : undefined Just Logged In
-console.log(notification1);
-console.log(notification2);
-console.log(notification3);
+// console.log(notification1);
+// console.log(notification2);
+// console.log(notification3);
 
-function loginUserMessage1(username){
+function loginUserMessage1(username = "User"){ // WE CAN GIVE DEFAULT VALUE IF NOTHING IS PASSED BY THE USER
     if(username === undefined){   // CAN ALSO WRITE if(!username){......}, ITS SAME AS WHAT WE DID IN THIS LINE.
         console.log("please enter a username.");
         return;
@@ -59,4 +59,4 @@ function loginUserMessage1(username){
     return `${username} Just Logged In`;
 }
 
-console.log(loginUserMessage1());
+console.log(loginUserMessage1("Ashish"));
