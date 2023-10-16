f0655af7c88482880314902865362374f6e40b0b
Ashish Singh <singh.ashish041@gmail.com>
2023-10-16 08:29:06 +0530

    learning about arrow function

--- INTERMEDIATE/08_functions/03_function.js
+++ INTERMEDIATE/08_functions/03_function.js
@@ -10,8 +10,28 @@ const user = {
     }
 }
 
-user.welcomeMsg();
-user.username = "Sam";
-user.welcomeMsg();
+// user.welcomeMsg();
+// user.username = "Sam";
+// user.welcomeMsg();
 
-console.log(this); // THIS RETURNS THE VALUE OF AN EMPTY OBJECT IN NODE ENVIRONMENT || IN BROWSER SAME CODE RETURN WINDOW SOMETHING.
+// console.log(this); // THIS RETURNS THE VALUE OF AN EMPTY OBJECT IN NODE ENVIRONMENT BECAUSE IN NODE ENVIRONMENT GLOBAL OBJECT IS EMPTY OBJECT || IN BROWSER SAME CODE RETURN WINDOW OBJECT SOMETHING BECAUSE WINDOW IS A GLOBAL OBJECT
+
+// function chai(){
+//     let username = "Ashish";
+//     console.log(this.username); // THIS KEYWORD ONLY WORKS IN THE OBJECT NOT IN THE FUNCTION.
+// }
+
+// chai();
+
+// const chai = function(){
+//     let username = "Ashish";
+//     console.log(this.username); // STILL GIVING UNDEFINED.
+// }
+
+// ARROW FUNCTION
+const chai = () => { // JUST FUNCTION KEYWORD IS REMOVED FORM THE ABOVE FUNCTION DECLARATION.
+    let username = "Ashish";
+    console.log(this.username); // STILL GIVING UNDEFINED.
+}
+
+chai();
