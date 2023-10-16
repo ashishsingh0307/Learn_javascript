6df4230011e40071e7d1e808b53d6c778cd53b26
Ashish Singh <singh.ashish041@gmail.com>
2023-10-16 21:59:16 +0530

    learning more about invoked functions

--- INTERMEDIATE/08_functions/04_function.js
+++ INTERMEDIATE/08_functions/04_function.js
@@ -1,2 +1,17 @@
 // IMMEDIATLY INVOKED FUNCTION EXPRESSION (IIFE)
 
+(function chai(){
+    console.log("DB Connected ONE");
+}) (); // THIS IS IIFE SYNTAX || MANY TIMES GLOBAL SCOPE POLLUTION HAPPENS THEN WE USE IIFE SO THAT WE CAN REMOVE THAT GLOBAL SCOPE POLLUTION.
+
+(function chai2(){
+    console.log("DB CONNECTED TWO");
+}) (); // THIS CAUSES ERROR BECAUSE WHEN EVER WE USE IIFE WE HAVE TO END THAT INVOKED FUNCTION WITH A SEMI COLON (;).
+
+(() => {
+    console.log("DB CONNECTED THREE");
+}) ();
+
+((name) => {
+    console.log(`DB CONNECTED BY ${name}`);
+}) ("ASHISH");
