f5e9a1d1082d121f002d15563880b242a7842fe0
Ashish Singh <singh.ashish041@gmail.com>
2023-10-16 22:17:56 +0530

    learning about JEC

--- INTERMEDIATE/10_Execution/01_execute.js
+++ INTERMEDIATE/10_Execution/01_execute.js
@@ -1,1 +1,14 @@
 // JAVASCRIPT EXECUTION CONTEXT
+
+/*
+
+    -> Javascript divides your program or code in 2 Phases
+
+        => Whenever JS executes your code it makes a Global Execution Context(GEC). (whenever this keyword is used in a global scope it will give you GEC).
+            When we use browser and type console.log(this); it will give use window object.
+            when run in node environment it gives us blank object.
+            etc.
+
+        =>
+
+*/
