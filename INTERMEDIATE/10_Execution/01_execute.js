1f21ba3df63aad3a473ffd7a860f5c815b406ae4
Ashish Singh <singh.ashish041@gmail.com>
2023-10-17 08:26:48 +0530

    learning about GEC

--- INTERMEDIATE/10_Execution/01_execute.js
+++ INTERMEDIATE/10_Execution/01_execute.js
@@ -5,15 +5,18 @@
     -> Javascript divides your program or code in 2 Phases
 
         => Whenever JS executes your code it makes a Global Execution Context(GEC). (whenever this keyword is used in a global scope it will give you GEC).
-            When we use browser and type console.log(this); it will give use window object.
-            when run in node environment it gives us blank object.
+            - When we use browser and type console.log(this); it will give use window object.
+            - When run in node environment it gives us blank object.
             etc.
 
             There are 3 Types of Execution Context
                 1) Global Execution Context.
-                2) Function Execution Context.
+                2) Function/Functional Execution Context.
                 3) Eval Execution Context.
 
-        =>
+        => Javascript divides your program or code in 2 Phases which are as follows
+
+            - Memory Creation Phase / Memory Phase
+            -
 
 */
