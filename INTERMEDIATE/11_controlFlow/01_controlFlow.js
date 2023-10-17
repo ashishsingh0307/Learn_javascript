c2336619c5231899f5d97ee6eb98c6b644b43be7
Ashish Singh <singh.ashish041@gmail.com>
2023-10-17 19:41:43 +0530

    learning about conteol flow statements

--- INTERMEDIATE/11_controlFlow/01_controlFlow.js
+++ INTERMEDIATE/11_controlFlow/01_controlFlow.js
@@ -1,7 +1,14 @@
 const isUserLoggedIn = true;
 const temprature = 41;
 
+// if(temprature < 50){
+//     console.log("less than 50"); // THIS LINE WILL BE EXECUTED IF THE CONDITION IS TRUE
+// }
+// console.log("more than 50"); // THIS LINE WILL BE EXECUTED EVEN IF CONDITION IS FALSE BECAUSE IT LIES OUTSIDE THE SCOPE OF IF ELSE STATEMENT.
+
 if(temprature < 50){
-    console.log("less than 50"); // T
+    console.log("less than 50"); // NOW IF CONDITION IS TRUE THEN THIS CODE BLOCK WILL BE EXECUTED.
+}else{
+    console.log("More than 50"); // AND IF THE CONDITION IS FALSE THEN THIS CODE BLOCK WILL BE EXECUTED
 }
-console.log("more than 50");
+
