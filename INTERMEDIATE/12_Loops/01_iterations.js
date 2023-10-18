00a63548a7d7928f1d5a3cd0cc8ffb11b41369b3
Ashish Singh <singh.ashish041@gmail.com>
2023-10-18 08:58:48 +0530

    learning about loops

---
+++ INTERMEDIATE/12_Loops/01_iterations.js
@@ -0,0 +1,19 @@
+// FOR LOOP
+
+// SYNTAX ::
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+// }
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if(element == 5){
+        console.log("5 is the best number");
+    }
+    console.log(element);
+}
