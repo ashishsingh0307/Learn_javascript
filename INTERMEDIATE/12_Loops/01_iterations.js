3d1c21a51f1a37d89ec69295ce5811e024900034
Ashish Singh <singh.ashish041@gmail.com>
2023-10-18 19:20:48 +0530

    learned about break keyword

--- INTERMEDIATE/12_Loops/01_iterations.js
+++ INTERMEDIATE/12_Loops/01_iterations.js
@@ -34,10 +34,26 @@ for (let i = 1; i <= 10; i++) {
 }
 
 let myArray = ["Flash", "Batman", "Superman"];
-console.log(myArray.length);
+// console.log(myArray.length);
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
-    console.log(element);
+    // console.log(element);
 }
 
 // BREAK AND CONTINUE
+
+for (let i = 1; i <= 20; i++) {
+    if(i == 5){
+        console.log("Detected Number 5");
+        break;
+    }
+    console.log(`Value of i is : ${i}`);
+}
+
+for (let i = 1; i <= 20; i++) {
+    if(i == 5){
+        console.log("Detected Number 5");
+        break;
+    }
+    console.log(`Value of i is : ${i}`);
+}
