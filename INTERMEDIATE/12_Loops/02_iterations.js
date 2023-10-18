abf84c0198185ec3365dae17ac4a2f5de7edddf7
Ashish Singh <singh.ashish041@gmail.com>
2023-10-18 19:34:39 +0530

    learned about while loop

---
+++ INTERMEDIATE/12_Loops/02_iterations.js
@@ -0,0 +1,19 @@
+// WHILE LOOP
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of Index is ${index}`);
+    index+=2; // OR WE CAN WRITE IT AS {index = index + 2}
+}
+
+let myArray = ["flash", "batman", "superman"]
+let arr = 0;
+
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr+=1;
+}
+
+
+// DO WHILE LOOP
+
