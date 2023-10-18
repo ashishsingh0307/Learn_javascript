0eceea7a1bad1a65fa28fcfac32da1b4a0d8dddc
Ashish Singh <singh.ashish041@gmail.com>
2023-10-18 19:11:42 +0530

    using array and for loop together

--- INTERMEDIATE/12_Loops/01_iterations.js
+++ INTERMEDIATE/12_Loops/01_iterations.js
@@ -26,9 +26,16 @@ for (let i = 0; i < 3; i++) {
 }
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`Table for ${i}`);
+    // console.log(`Table for ${i}`);
     for (let j = 1; j <= 10; j++) {
-        console.log(`${i} * ${j} = ${i*j}`);
+        // console.log(`${i} * ${j} = ${i*j}`);
     }
 
+}
+
+let myArray = ["Flash", "Batman", "Superman"];
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
 }
