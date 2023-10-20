4d0592cf24e2e1581e90c152fd1e95a16aa2a2fb
Ashish Singh <singh.ashish041@gmail.com>
2023-10-20 09:00:46 +0530

    learned more about filters

--- INTERMEDIATE/12_Loops/04_iterations.js
+++ INTERMEDIATE/12_Loops/04_iterations.js
@@ -13,5 +13,15 @@ const myNums = [1,2,3,4,5,6,7,8,9,10];
 const newNums = myNums.filter((num)=> num >= 5); // FILTER ONLY WORK WHEN WE ASSIGN IT TO A VARIABLE.
 // const newNums = myNums.filter((num)=> {num >= 5}); || THIS DID NOT WORK BECASUE WHEN WE USE CURLY BRACKETS WE STARTED A SCOPE ADN WHENEVER WE START A SCOPE WE NEED TO RETURN A VALUE {EXPLAINED IN 03_FUNCTION.JS FILE}
 const newNums1 = myNums.filter((num)=>{ return num <= 5 })
-console.log(newNums);
-console.log(newNums1);
+
+const newNums2 = [];
+
+myNums.forEach((num)=>{
+    if(num > 4){
+        newNums2.push(num);
+    }
+})
+
+console.log(`This is using filter function : ${newNums}`);
+console.log('This is using filter function :', newNums1);
+console.log(`This is using Foreach Loop : ${newNums2}`);
