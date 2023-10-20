6c2cc3761e849ad4b8421001efc60cd9aff86e51
Ashish Singh <singh.ashish041@gmail.com>
2023-10-20 08:55:19 +0530

    learning about filter and returning values

--- INTERMEDIATE/12_Loops/04_iterations.js
+++ INTERMEDIATE/12_Loops/04_iterations.js
@@ -10,5 +10,8 @@
 const myNums = [1,2,3,4,5,6,7,8,9,10];
 
 // FILTER FUNCTION IS ALSO SIMILAR TO FOREACH LOOP WHICH REQUIRES A CALLBACK FUNCTION.
-const newNums = myNums.filter((num)=> num > 4); // FILTER ONLY WORK WHEN WE ASSIGN IT TO A VARIABLE.
+const newNums = myNums.filter((num)=> num >= 5); // FILTER ONLY WORK WHEN WE ASSIGN IT TO A VARIABLE.
+// const newNums = myNums.filter((num)=> {num >= 5}); || THIS DID NOT WORK BECASUE WHEN WE USE CURLY BRACKETS WE STARTED A SCOPE ADN WHENEVER WE START A SCOPE WE NEED TO RETURN A VALUE {EXPLAINED IN 03_FUNCTION.JS FILE}
+const newNums1 = myNums.filter((num)=>{ return num <= 5 })
 console.log(newNums);
+console.log(newNums1);
