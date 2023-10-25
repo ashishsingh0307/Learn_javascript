c4ee225bcb6b26b4531c2c6ca2a69c09dcc9a8c1
Ashish Singh <singh.ashish041@gmail.com>
2023-10-25 08:20:55 +0530

    learned maps

--- INTERMEDIATE/12_Loops/05_iterations.js
+++ INTERMEDIATE/12_Loops/05_iterations.js
@@ -1,7 +1,15 @@
 const myNumbers = [1,2,3,4,5,6,7,8,9,10];
 
-const myNums = myNumbers.map((num)=> num + 10);
+// const myNums = myNumbers.map((num)=> num + 10);
 
-console.log(myNums);
+// const newNums = myNumbers.map().map().filter(); // THIS IS CALLED CHAINING METHOD
+const newNums = myNumbers
+                    .map((num) => num*10) // FIRST MAP FUNCTION WILL MULTIPLY EVERY ELEMENT IN ARRAY WITH THE 10
+                    .map((num) => num+1)  // SECOND MAP FUNCTION WILL ADD 1 TO EVERY ELEMENT { THE BASE NUMBER WILL BE THE RESULT FROM THE FIRST MAP FUNCTION i.e. FIRST ELEMENT WILL BE 10 NOW } || MAP FUNCTION RETURN EXPLICITLY
+                    .filter((num) => num >= 40) // THIS IS A FILTER FUNCTION THAT USED IN CHAIN || FILTER RETURN ONLY TRUE AND FALSE
+
+console.log(newNums);
+
+
 
 // video 30 19:23
