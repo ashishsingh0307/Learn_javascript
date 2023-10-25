c569d7f7d7573cb43af4e060c136ca82a45f2e7a
Ashish Singh <singh.ashish041@gmail.com>
2023-10-25 08:35:18 +0530

    laerned about reducer

--- INTERMEDIATE/12_Loops/06_iterations.js
+++ INTERMEDIATE/12_Loops/06_iterations.js
@@ -2,3 +2,11 @@
 
 const myNums = [1,2,3];
 
+// const myTotal = myNums.reduce((acc, currval)=>{
+//     console.log(`accumulator : ${acc} and current value : ${currval}`);
+//     return acc + currval;
+// }, 0);
+
+const myTotal = myNums.reduce((acc, curr) => acc + curr, 0)
+
+console.log(myTotal);
