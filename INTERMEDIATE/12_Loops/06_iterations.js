091d1a26c9d7f5bd6a09957918b4eaf9779797e6
Ashish Singh <singh.ashish041@gmail.com>
2023-10-25 08:41:29 +0530

    practiced an example on reducer

--- INTERMEDIATE/12_Loops/06_iterations.js
+++ INTERMEDIATE/12_Loops/06_iterations.js
@@ -9,4 +9,29 @@ const myNums = [1,2,3];
 
 const myTotal = myNums.reduce((acc, curr) => acc + curr, 0)
 
-console.log(myTotal);
+// console.log(myTotal);
+
+// SHOPPING CART
+
+const shoppingCart = [
+    {
+        itemName : "JS Course",
+        price : 2999
+    },
+    {
+        itemName : "Python Course",
+        price : 3099
+    },
+    {
+        itemName : "CPP Course",
+        price : 3999
+    },
+    {
+        itemName : "Data Science Course",
+        price : 12999
+    }
+]
+
+const priceToPay = shoppingCart.reduce((acc, item)=> acc + item.price, 0);
+
+console.log(priceToPay);
